0978e39b6cfc030c71dbffc6324506aa99ddf86f
Oleg <81077694+ismate@users.noreply.github.com>
2026-04-22 02:05:37 +0300

    Update test_users.py

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,19 +1,6 @@
 from api.users_api import get_user, patch_user
 
 
-def test_user_with_headers(base_url, create_user):
-    user_id = create_user["id"]
-
-    headers = {
-        "Authorization": "Bearer testtoken123"
-    }
-
-    get_response = get_user(base_url, user_id, headers=headers)
-
-    assert get_response.status_code == 200
-    assert get_response.json()["id"] == user_id
-
-
 def test_get_user_with_valid_headers(base_url, create_user, auth_headers):
     user_id = create_user["id"]
 
@@ -22,21 +9,16 @@ def test_get_user_with_valid_headers(base_url, create_user, auth_headers):
     assert response.status_code == 200
     assert response.json()["id"] == user_id
 
-    
-def test_unexisting_user(base_url):
+
+def test_patch_non_existing_user(base_url, auth_headers):
     update_payload = {
         "name": "Ivan"
     }
 
-    headers = {
-        "Authorization": "Bearer testtoken123"
-    }
+    response = patch_user(base_url, 999, update_payload, headers=auth_headers)
 
-    patch_response = patch_user(base_url, 999, update_payload, headers=headers)
-
-    assert patch_response.status_code == 404
-    data = patch_response.json()
-    assert "detail" in data
+    assert response.status_code == 404
+    assert "detail" in response.json()
 
 
 def test_patch_user_with_invalid_data(base_url, create_user, auth_headers):
