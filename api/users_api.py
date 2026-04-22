e7caa5e1cb1110e29fbc88283ecb1a638b811197
Oleg <81077694+ismate@users.noreply.github.com>
2026-04-23 02:07:07 +0300

    Update users_api.py

--- api/users_api.py
+++ api/users_api.py
@@ -9,9 +9,9 @@ def get_user(base_url, user_id, headers=None):
     return requests.get(f"{base_url}/users/{user_id}", headers=headers)
 
 
-def update_user(base_url, user_id, payload):
-    return requests.patch(f"{base_url}/users/{user_id}", json=payload)
+def patch_user(base_url, user_id, payload, headers=None):
+    return requests.patch(f"{base_url}/users/{user_id}", json=payload, headers=headers)
 
 
-def delete_user(base_url, user_id):
-    return requests.delete(f"{base_url}/users/{user_id}")
+def delete_user(base_url, user_id, headers=None):
+    return requests.delete(f"{base_url}/users/{user_id}", headers=headers)
